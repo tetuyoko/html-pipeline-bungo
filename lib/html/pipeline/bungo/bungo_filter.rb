6775bae0ebabacf00ca051af146a188b2980392e
tetuyoko <tyokoyama53@gmail.com>
2016-07-05 15:15:04 +0900

    name fixed

--- lib/html/pipeline/bungo/bungo_filter.rb
+++ lib/html/pipeline/bungo/bungo_filter.rb
@@ -3,36 +3,36 @@ module HTML
     class BungoFilter < TextFilter
       def call
         html = @text
-        html = ruby_fliter(html)
-        html = emph_dot_fliter(html)
-        html = bold_fliter(html)
-        html = paragraph_fliter(html)
+        html = ruby_filter(html)
+        html = emph_dot_filter(html)
+        html = bold_filter(html)
+        html = paragraph_filter(html)
         html.rstrip!
         html
       end
 
       private
 
-      def ruby_fliter(text)
+      def ruby_filter(text)
         text.gsub(/:([^:]+?)\(([^:]+?)\)/) do |match|
           "<ruby><rb>#{$1}</rb><rp>(</rp><rt>#{$2}</rt><rp>)</rp></ruby>"
         end
       end
 
-      def emph_dot_fliter(text)
+      def emph_dot_filter(text)
         text.gsub(/:([^:]+?)\.\./) do |match|
           literals = $1.split(//).map{|lt| "<span class='boten'>#{lt}</span>"}
           "<span class='botenparent'>#{literals.join}</span>"
         end
       end
 
-      def bold_fliter(text)
+      def bold_filter(text)
         text.gsub(/:([^:]+?)\!/) do |match|
           "<b>#{$1}</b>"
         end
       end
 
-      def paragraph_fliter(text)
+      def paragraph_filter(text)
         "<p>#{text.gsub(/\r\n|\r|\n/, "</p><p>")}</p>"
       end
 
